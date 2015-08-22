71ac53187f25459df7a274b8948cfed817768b6b
Karol Kaczmarek <karol@MacBook-Pro.local>
2015-08-22 10:44:00 +0200

    added progress bar

--- src/rsync/rsync.js
+++ src/rsync/rsync.js
@@ -1,11 +1,16 @@
 var Rsync = require('rsync');
 var logger = require('../utils/log.js');
+var ProgressBar = require('progress');
+
 
 
 var Command = function(dir){
   this.rsync = new Rsync()
       .shell('ssh -p '+dir.getTarget().getSsh().getPort())
-      .flags('az')
+      .flags('a')
+      // .set('progress')
+      .set('info', 'progress2')
+      // .set('stats')
       .source(dir.getPath())
       .destination(dir.getTarget().getSsh().getUser()
           + '@'
@@ -17,13 +22,41 @@ var Command = function(dir){
 };
 
 Command.prototype.exec = function(){
+  var total, remain,
+      regexp = /to-chk=(\d+)/,
+      progress;
+
+  // logger.log('executing', this.rsync.command());
+  this.rsync.output(function(data){
+    var result = data.toString().match(regexp);
+    if(!total && result && result[1]){
+      total = parseInt(result[1]);
+      progress = new ProgressBar('syncing [:bar] :percent :etas', {
+        total: total,
+        complete: '=',
+        width: 20,
+        callback: function(){console.log('\n')}
+      });
+    }
+    if(total && result && result[1]){
+        remain = parseInt(result[1]);
+        progress.tick(total - remain);
+        total = remain;
+
+    } else {
+      // console.log(data.toString());
+    }
+  });
   this.rsync.execute(function(error, code, cmd){
+      total = null;
+      remain = null;
       if(code === 0){
-        logger.log('sync complete.');
+        setTimeout(function(){
+            logger.log('sync complete.');
+        }, 10);
       } else {
         logger.log(error, cmd);
       }
-
   });
 }
 
